5eb8fd158ff8e289aa0eaf2e49bbe8eb648d5db9
Anders Refsdal Olsen <andersro93@gmail.com>
2023-03-09 20:17:17 +0100

    Refactored project and upgraded to newest dotnet

--- Program.cs
+++ Program.cs
@@ -2,8 +2,34 @@
 
 var builder = WebApplication.CreateBuilder(args);
 
-var app = builder
-    .ConfigureServices()
-    .ConfigurePipeline();
+builder.Services.AddRazorPages();
+
+var isBuilder = builder.Services.AddIdentityServer(options =>
+    {
+        options.Events.RaiseErrorEvents = true;
+        options.Events.RaiseInformationEvents = true;
+        options.Events.RaiseFailureEvents = true;
+        options.Events.RaiseSuccessEvents = true;
+
+        // see https://docs.duendesoftware.com/identityserver/v6/fundamentals/resources/
+        options.EmitStaticAudienceClaim = true;
+    })
+    .AddTestUsers(TestUsers.Users);
+
+// in-memory, code config
+isBuilder.AddInMemoryIdentityResources(Config.IdentityResources);
+isBuilder.AddInMemoryApiScopes(Config.ApiScopes);
+isBuilder.AddInMemoryClients(Config.Clients);
+
+var app = builder.Build();
+
+app.UseDeveloperExceptionPage();
+app.UseStaticFiles();
+app.UseRouting();
+app.UseIdentityServer();
+app.UseAuthorization();
+        
+app.MapRazorPages()
+    .RequireAuthorization();
 
 app.Run();

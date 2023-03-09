fc4ee0787ce20118f16e0b576d2d84b16a371e02
Anders Refsdal Olsen <andersro93@gmail.com>
2023-03-09 21:21:33 +0100

    Add fake user base that is seeded at startup

---
+++ Factories/UsersFactory.cs
@@ -0,0 +1,27 @@
+using System.Security.Claims;
+using Bogus;
+using Bogus.Extensions.Norway;
+using Duende.IdentityServer.Test;
+using IdentityModel;
+
+namespace TestIdp.Factories;
+
+public static class UsersFactory
+{
+    public static Faker<TestUser> Faker => new Faker<TestUser>("nb_NO")
+        .RuleFor(u => u.SubjectId, faker => faker.Random.Guid().ToString())
+        .RuleFor(u => u.Username, faker => faker.Person.Email)
+        .RuleFor(u => u.Password, faker => faker.Internet.Password(faker.Random.Int(4, 16)))
+        .RuleFor(u => u.IsActive, faker => true)
+        .RuleFor(u => u.Claims, faker => new List<Claim>()
+        {
+            new ("ssn", faker.Person.Fødselsnummer()),
+            new (JwtClaimTypes.Name, $"{faker.Person.LastName}, {faker.Person.FirstName}"),
+            new (JwtClaimTypes.GivenName, faker.Person.FirstName),
+            new (JwtClaimTypes.FamilyName, faker.Person.LastName),
+            new (JwtClaimTypes.Email, faker.Person.Email),
+            new (JwtClaimTypes.EmailVerified, "true", ClaimValueTypes.Boolean),
+            new (JwtClaimTypes.PhoneNumber, faker.Phone.PhoneNumberFormat(3).Replace(" ", "")), // Creates E164 numbers
+            new (JwtClaimTypes.PhoneNumberVerified, "true", ClaimValueTypes.Boolean),
+        });
+}
--- Program.cs
+++ Program.cs
@@ -1,25 +1,29 @@
-﻿using TestIdp;
+﻿using Bogus;
+using TestIdp;
+using TestIdp.Factories;
+
+// The seed that is used to generate users
+Randomizer.Seed = new Random(1337);
+var testUsers = UsersFactory.Faker.Generate(200);
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddRazorPages();
 
 var isBuilder = builder.Services.AddIdentityServer(options =>
-    {
-        options.Events.RaiseErrorEvents = true;
-        options.Events.RaiseInformationEvents = true;
-        options.Events.RaiseFailureEvents = true;
-        options.Events.RaiseSuccessEvents = true;
+{
+    options.Events.RaiseErrorEvents = true;
+    options.Events.RaiseInformationEvents = true;
+    options.Events.RaiseFailureEvents = true;
+    options.Events.RaiseSuccessEvents = true;
+});
 
-        // see https://docs.duendesoftware.com/identityserver/v6/fundamentals/resources/
-        options.EmitStaticAudienceClaim = true;
-    })
-    .AddTestUsers(TestUsers.Users);
-
-// in-memory, code config
+isBuilder.AddTestUsers(testUsers);
 isBuilder.AddInMemoryIdentityResources(Config.IdentityResources);
 isBuilder.AddInMemoryApiScopes(Config.ApiScopes);
 isBuilder.AddInMemoryClients(Config.Clients);
+
+builder.Services.AddSingleton(testUsers);
 
 var app = builder.Build();
 
